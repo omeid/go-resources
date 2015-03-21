87e02d1bea4291bfcf4575bc4572a1ebea0f38d3
Mohamedh Fazal <mohamedhfazal@gmail.com>
2015-03-21 17:50:36 +0500

    Add flag to remove given prefix from the resulting path.

--- cmd/resources/cmd.go
+++ cmd/resources/cmd.go
@@ -6,16 +6,18 @@ import (
 	"flag"
 	"log"
 	"path/filepath"
+	"strings"
 
 	"github.com/omeid/go-resources"
 )
 
 var (
-	Pkg     = flag.String("package", "main", "The name of package to generate.")
-	Var     = flag.String("var", "FS", "The name of variable to assign the virtual-filesystem to.")
-	Tag     = flag.String("tag", "", "The tag to use for the generated package. Use empty for no tag.")
-	Declare = flag.Bool("declare", false, "Whether to declare the \"var\" or not.")
-	Out     = flag.String("output", "", "The filename to write the output to.")
+	Pkg      = flag.String("package", "main", "The name of package to generate.")
+	Var      = flag.String("var", "FS", "The name of variable to assign the virtual-filesystem to.")
+	Tag      = flag.String("tag", "", "The tag to use for the generated package. Use empty for no tag.")
+	Declare  = flag.Bool("declare", false, "Whether to declare the \"var\" or not.")
+	Out      = flag.String("output", "", "The filename to write the output to.")
+	TrimPath = flag.String("trim", "", "Path prefix to remove from the resulting file path")
 )
 
 type nope struct{}
@@ -47,7 +49,8 @@ func main() {
 	}
 
 	for file, _ := range files {
-		err := res.AddFile(file, file)
+		path := strings.TrimPrefix(file, *TrimPath)
+		err := res.AddFile(file, path)
 		if err != nil {
 			log.Fatal(err)
 		}
